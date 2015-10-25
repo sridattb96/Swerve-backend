9119a82738c8b142bac055501991d9a033d28cc7
Sridatt Bhamidipati <sbhamidi@ucdavis.edu>
2015-10-25 02:20:16 -0700

    heroku

--- config/development.js
+++ config/development.js
@@ -1,5 +1,6 @@
 module.exports = {
  	// local mongodb url
-    db : 'mongodb://localhost/money2020',
+    //db : 'mongodb://localhost/money2020',
+    db : 'mongodb://money2020:money2020@ds045454.mongolab.com:45454/swerve-db',
     sessionSecret : 'secret-session-var'
 }
